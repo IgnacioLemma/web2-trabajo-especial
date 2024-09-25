c05a0d0fda1921a03e6adc0851d3ce02ca8edc36
Ignacio Lemma <ignaciooleemma@gmail.com>
2024-09-25 15:41:41 -0300

    Credeado V.1.0.0 Router

---
+++ router.php
@@ -0,0 +1,43 @@
+<?php
+// require_once 'app/controllers/hostel.controller.php';
+
+// base_url para redirecciones y base tag
+define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+$action = 'listar'; // accion por defecto si no se envia ninguna
+if (!empty( $_GET['action'])) {
+    $action = $_GET['action'];
+}
+
+// tabla de ruteo
+
+// listar items  -> HostelController->showRoom();
+// detalle items  -> HostelController->showRoomDetails();
+// listar categorias -> HostelController->showListCategory();
+// listar items x categoria -> HostelController -> showItemsCategory();
+
+
+// parsea la accion para separar accion real de parametros
+$params = explode('/', $action);
+
+switch ($params[0]) {
+    case 'Rooms':
+        $controller = new HostelController();
+        $controller->showRoom();
+        break;
+    case 'RoomsDetails':
+        $controller = new HostelController();
+        $controller->showRoomDetails();
+        break;
+    case 'ListCatergory':
+        $controller = new HostelController();
+        $controller->showListCategory();
+        break;
+    case 'ItemsCatergory':
+        $controller = new HostelController();
+        $controller->showItemsCategory();
+        break;
+    default: 
+        echo "404 Page Not Found"; // pagina de error por hacer
+        break;
+}
