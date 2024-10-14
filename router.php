081f45fe08b2fb9a9dc5eda52bcd238aaf53c6aa
Edeenita <ignaciooleemma@gmail.com>
2024-10-14 20:59:17 -0300

    Update router.php

--- router.php
+++ router.php
@@ -11,7 +11,7 @@ define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] 
 
 $res = new Response(); // Utiliza las respuestas HTTP
 
-$action = 'Rooms'; // Acción por defecto si no se envía ninguna
+$action = 'Home'; // Acción por defecto si no se envía ninguna
 if (!empty($_GET['action'])) {
     $action = $_GET['action'];
 }
